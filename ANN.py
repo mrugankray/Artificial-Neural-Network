87b47eadc9cde2741424a12a3fb7ca7843887dd2
mrugankray <mrugankray2@gmail.com>
2018-07-29 23:11:54 +0530

    first commit

---
+++ ANN.py
@@ -0,0 +1,65 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]#to avoid falling into dummy variable trap
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#import keras
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+#intialize the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+
+# Adding the input layer and the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the input layer and the second output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+#if dependent variable has more than two types of categories than we should use loss='softmax'
+
+#compiling the ANN
+classifier.compile(optimizer = 'adam', loss='binary_crossentropy', metrics = ['accuracy'])
+#if dependent variable has more than two kind of valuea than we should use loss='categorical_crossentropy'
+
+#fitting the dataset into trainig set and test set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#accuracy
+accuracy = (1528+192)/(1528+213+67+192)
